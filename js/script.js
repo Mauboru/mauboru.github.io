eb767f98cd9579e6c4c935fefcccf5878b1e19aa
Josue Henrique <josue21henrique@gmail.com>
2024-04-17 16:23:11 -0300

    update about language for english

--- js/script.js
+++ js/script.js
@@ -67,6 +67,12 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".home h3").innerHTML = "Eu sou um <span class='multiple-text'>Violinista</span>";
         document.querySelector(".home p").textContent = "Sou Josué, um Developer apaixonado por transformar ideias em experiências cativantes. Navegue pelo meu portfólio para explorar uma coleção única de projetos que refletem minha dedicação à inovação, design e criatividade.";
         document.querySelector(".home .btn").textContent = "Baixar Curriculo";
+
+        document.querySelector(".about h2").innerHTML = "Sobre <span>Mim</span>";
+        document.querySelector(".about h3").textContent = "Violinista e Programador!";
+        document.querySelector(".about .p1").textContent = "Desde pequeno tive uma paixão muito grande pela música e tecnologia!";
+        document.querySelector(".about .p2").textContent = "Com o passar do tempo fui me especializando nas duas áreas, comecei estudando violão, depois outros instrumentos e então fui para o violino, onde depositei todo meu esforço e dedicação deixando assim de lado os outros instrumentos com o intuito de me profissionalizar."
+        document.querySelector(".about .p3").textContent = "Me dediquei também a programação, começando com projetos de jogos 'Drag and Drop' e então evoluindo para então outras engines e só então as linguagens de programação.";
     });
 
     document.querySelector(".en").addEventListener("click", function() {
@@ -82,5 +88,11 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector(".home h3").innerHTML = "I'm a <span class='multiple-text'>Python Developer</span>";
         document.querySelector(".home p").textContent = "I'm Josué, a Developer passionate about transforming ideas into captivating experiences. Browse my portfolio to explore a unique collection of projects that reflect my dedication to innovation, design and creativity.";
         document.querySelector(".home .btn").textContent = "Download Curriculum";
+
+        document.querySelector(".about h2").innerHTML = "About <span>Me</span>";
+        document.querySelector(".about h3").textContent = "Violinist and Programmer!";
+        document.querySelector(".about .p1").textContent = "Since I was little, I've had a huge passion for music and technology!";
+        document.querySelector(".about .p2").textContent = "As time went by, I specialized in both areas, I started studying guitar, then other instruments and then I switched to violin, where I put all my effort and dedication aside, leaving other instruments aside with the aim of becoming a professional."
+        document.querySelector(".about .p3").textContent = "I also dedicated myself to programming, starting with 'Drag and Drop' game projects and then evolving to other engines and only then programming languages.";
     });
 });
