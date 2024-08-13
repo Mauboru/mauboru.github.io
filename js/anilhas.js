df26ae318760c2ae2bcca7e66abf57d39f5c8014
mauboru <josue21henrique@gmail.com>
2024-08-12 21:21:00 -0300

    test

--- js/anilhas.js
+++ js/anilhas.js
@@ -1,35 +1,35 @@
-function formatDateTime(dateTime) {
-    const date = new Date(dateTime);
-    const pad = (num) => num.toString().padStart(2, '0');
-    
-    const day = pad(date.getDate());
-    const month = pad(date.getMonth() + 1);
-    const year = date.getFullYear();
-    const hours = pad(date.getHours());
-    const minutes = pad(date.getMinutes());
-    const seconds = pad(date.getSeconds());
-    
-    return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
-}
-
-setInterval(function () {
-    $.get("http://85.31.63.241:3000/anilhas", function(data) {
-        let obj = data;
+    function formatDateTime(dateTime) {
+        const date = new Date(dateTime);
+        const pad = (num) => num.toString().padStart(2, '0');
         
-        $('#table > tbody').empty();
-        $('#table > caption').text("0" + obj.length + " AVE(S) NO NINHO");
+        const day = pad(date.getDate());
+        const month = pad(date.getMonth() + 1);
+        const year = date.getFullYear();
+        const hours = pad(date.getHours());
+        const minutes = pad(date.getMinutes());
+        const seconds = pad(date.getSeconds());
+        
+        return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+    }
 
-        obj.forEach(function(dev) {
-            let person = dev.anilha;
-            if (dev.nome != null) {
-                person = dev.nome;
-            }
+    setInterval(function () {
+        $.get("https://85.31.63.241:3000/anilhas", function(data) {
+            let obj = data;
             
-            const formattedDate = formatDateTime(dev.entrada);
+            $('#table > tbody').empty();
+            $('#table > caption').text("0" + obj.length + " AVE(S) NO NINHO");
 
-            $('#table > tbody').append(
-                `<tr><td class='text-center'><b>${person}</b></td><td class='d-none d-md-table-cell text-center'><b>${formattedDate}</b></td></tr>`
-            );
+            obj.forEach(function(dev) {
+                let person = dev.anilha;
+                if (dev.nome != null) {
+                    person = dev.nome;
+                }
+                
+                const formattedDate = formatDateTime(dev.entrada);
+
+                $('#table > tbody').append(
+                    `<tr><td class='text-center'><b>${person}</b></td><td class='d-none d-md-table-cell text-center'><b>${formattedDate}</b></td></tr>`
+                );
+            });
         });
-    });
-}, 2000);  
+    }, 2000);  
